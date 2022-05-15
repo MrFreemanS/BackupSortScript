eeb3567c246b92613ad5620317d37cfa5584740f
Freeman <slavyan.free@gmail.com>
2022-05-15 13:29:40 +0300

    сделал обход по директориям

--- BackupSortScript/BackupSortScript.py
+++ BackupSortScript/BackupSortScript.py
@@ -1,20 +1,22 @@
-
 import os
-# определение текущей рабочей директории
-path = "C:/test"
+from tempfile import tempdir
+import tempfile
+startpath = "C:/test"
 
 DirAndTime = dict()
 
-# чтение записей
-with os.scandir(path) as listOfEntries:  
-    for entry in listOfEntries:
-        # печать всех записей, являющихся файлами
-        if entry.is_file():
-            tempPath = os.path.abspath(entry.name)
-            print(tempPath)
-            tempTime = os.path.getctime(tempPath)
-            print(tempTime)
-            tempDirAndTime = {tempPath: tempTime}
-            print(tempDirAndTime)
-            #DirAndTime={tempDirAndTime}
+def goToDir(dirpath):
+    with os.scandir(dirpath) as listOfEntries:  
+        for entry in listOfEntries:
+            if entry.is_file():
+                tempPath = os.path.abspath(entry.path)
+                #print(tempPath)
+                tempTime = os.path.getctime(tempPath)
+                #print(tempTime)            
+                tempDirAndTime = {tempPath: tempTime}
+                #print(tempDirAndTime)
+                DirAndTime.update(tempDirAndTime)
+
+
+gotodir(startpath)
 print(DirAndTime)
