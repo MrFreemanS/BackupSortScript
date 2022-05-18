29117d030693d94fd682c1acd72af690124f671d
adamches <adamartskhanov@gmail.com>
2022-05-18 15:56:37 +0300

    add day sort

--- BackupSortScript/BackupSortScript.py
+++ BackupSortScript/BackupSortScript.py
@@ -1,3 +1,5 @@
+
+import shutil
 import os
 import time
 import datetime
@@ -7,11 +9,21 @@ startpath = "C:/test"
 #target dir
 dstpath = "C:/dstTest"
 
+DayDir="C:/Day"
+
 #create dict for all files in source dir
 DirAndTime = dict()
 
+now = time.time()
+dt_now = datetime.datetime.fromtimestamp(now)
+SingleDay=86400
+SingleMonth=2629743
+SingleYear=31556926
+
+
 #collect data about files in dictionary
 def goToDir(dirpath):
+
     #go around files
     with os.scandir(dirpath) as listOfEntries:  
         for entry in listOfEntries:
@@ -23,15 +35,41 @@ def goToDir(dirpath):
             if entry.is_dir():
                 tempPath = os.path.abspath(entry.path)
                 goToDir(tempPath)
-
+ 
 
 def YearSort():
     for key, value in DirAndTime.items():
-        dt_c = datetime.datetime.fromtimestamp(value)
-        if dt_c.month == 12 and dt_c.day == 31:
-            print(dt_c)
+        #dt_c = datetime.datetime.fromtimestamp(value)
+       # if dt_c.month == 12 and dt_c.day == 31:
+        print (value)
+        #if (dt_c.day & dt_c.month & dt_c.year) == (dt_now.day & dt_now.month & dt_now.year) : 
+         #   print (dt_c)
+        #if dt_c.day == 28 :
+         #   print (dt_c)
+        #if
+        
+        #if (value-now)<=SingleDay:
+            
+        
+
+        
+        if (now-value)<=SingleDay:
+            shutil.move(key,DayDir)
+            print(now-value)
+                   
+            
+
+
+
+            
+
+
+
 
 goToDir(startpath)
 
-now = time.time()
+
+
+print(now)
+print (dt_now.day)
 YearSort()
