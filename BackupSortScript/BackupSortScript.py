af8c2f73f73e605d31ba633d6766f052327ebe64
adamches <adamartskhanov@gmail.com>
2022-05-26 14:10:02 +0300

    new1.0

--- BackupSortScript/BackupSortScript.py
+++ BackupSortScript/BackupSortScript.py
@@ -1,14 +1,8 @@
-
-from operator import mod
-import shutil,os,time,datetime,copy
+import shutil,os,time,datetime
 
 #source dir
 startpath = "C:/test"
-#target dir
-dstpath = "C:/dstTest"
-#путь для дневных бекапов
 DayDir="C:/Day"
-
 WeekDir="C:/Week"
 MonthDir="C:/Month"
 YearDir="C:/Year"
@@ -17,14 +11,8 @@ DirAndTime = dict()
 
 #Время в настоящий момент в юникс
 now = time.time()
-temp2=round(now)
-
-#перевод юникс в нормаль
-dt_now = datetime.datetime.fromtimestamp(now)
 # Юникс время за 1 день
 SingleDay=86400
-# Юникс время за 1 год
-SingleYear=31556926
 
 #collect data about files in dictionary
 def goToDir(dirpath):
@@ -46,7 +34,7 @@ def YearSort():
     for key, value in DirAndTime.items():
         dt_c = datetime.datetime.fromtimestamp(value)
         if dt_c.month == 12 and dt_c.day == 31:
-           shutil.move(key,YearDir)
+              shutil.move(key,YearDir)
         
       
 def DaySort():
@@ -57,12 +45,11 @@ def DaySort():
 
 def WeekSort():
     for key, value in DirAndTime.items():
-        temp1=round(value)
-        print(temp2-temp1)
         if  ((now-value) >=580775) & ((now-value) <= 753695) : 
             shutil.move(key,WeekDir)  # Перемещение файла в папку недельные отчеты "Week"
             # Сравнение разницы времени в реалтайм  времени создания файла с временем на 7 Дней в юникс формате
           # Перемещение файла в папку недельные отчеты "Week"
+
 def MonthSort():
     for key, value in DirAndTime.items():
         if  ((now-value) >=2417393.5280878544) & ((now-value) <= 2503552.6705605984) :
@@ -73,11 +60,9 @@ def MonthSort():
 
 
 goToDir(startpath)
-print (now)
 YearSort()
 DaySort() # Вызов функции 
 WeekSort()
 MonthSort()
 
-# Пофиксить баг в случае, когда дневной файл он же недельный, в случае переноса, а не копирования , перенесется только один
 # тоже самое с днями и месяцами
